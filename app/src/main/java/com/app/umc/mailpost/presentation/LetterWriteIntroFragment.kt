534fac47f87aebd91105c32a5dd1a53157d1b60a
tmdgh1592 <tmdgh1592@naver.com>
2022-08-10 12:21:52 +0900

    design: 편지 쓰기 레이아웃 작성

--- app/src/main/java/com/app/umc/mailpost/presentation/LetterWriteIntroFragment.kt
+++ app/src/main/java/com/app/umc/mailpost/presentation/LetterWriteIntroFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.app.umc.mailpost.databinding.FragmentLetterWriteBinding
+import com.app.umc.mailpost.databinding.FragmentLetterWriteIntroBinding
 
-class LetterWriteFragment : Fragment() {
-    lateinit var binding: FragmentLetterWriteBinding
+class LetterWriteIntroFragment : Fragment() {
+    lateinit var binding: FragmentLetterWriteIntroBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentLetterWriteBinding.inflate(inflater, container, false)
+        binding = FragmentLetterWriteIntroBinding.inflate(inflater, container, false)
         return binding.root
     }
 
